8b13178573d423145eb3773233f529ff27780bb7
Mihir Mehta <mihir.mehta@samsung.com>
2014-04-08 16:29:27 +0530

    Passes small input case, but not large input case.

--- code_jam_2013/round2/erdos_szekeres/1.cpp
+++ code_jam_2013/round2/erdos_szekeres/1.cpp
@@ -4,33 +4,33 @@ typedef std::vector<unsigned int> VU;
 typedef std::vector<VU> VVU;
 #include <cassert>
 
-void get_lt(unsigned int N, VU &A, VU &B, VVU &lt) {
+void get_gt(unsigned int N, VU &A, VU &B, VVU &gt) {
   VU last_A(N, 0);
   for (unsigned int i1 = 1; i1 <= N; ++i1) {
-    if (last_A[i1 - 1] >= 1) 
-      lt[last_A[i1 - 1] - 1].push_back(i1);
-    if (A[i1] >= 2) {
-      assert(last_A[A[i1] - 2] >= 1);
-      lt[i1 - 1].push_back(last_A[A[i1] - 2]);
+    if (last_A[A[i1 - 1] - 1] >= 1) 
+      gt[last_A[A[i1 - 1] - 1] - 1].push_back(i1);
+    if (A[i1 - 1] >= 2) {
+      assert(last_A[A[i1 - 1] - 2] >= 1);
+      gt[i1 - 1].push_back(last_A[A[i1 - 1] - 2]);
     }
-    last_A[A[i1] - 1] = i1;
+    last_A[A[i1 - 1] - 1] = i1;
   }
   VU last_B(N, N + 1);
   for (unsigned int i1 = N; i1 >= 1; --i1) {
-    if (last_B[i1 - 1] <= N)
-      lt[last_B[i1 - 1] - 1].push_back(i1);
-    if (B[i1] >= 2) {
-      assert(last_B[B[i1] - 2] <= N);
-      lt[i1 - 1].push_back(last_B[B[i1] - 2]);
+    if (last_B[B[i1 - 1] - 1] <= N)
+      gt[last_B[B[i1 - 1] - 1] - 1].push_back(i1);
+    if (B[i1 - 1] >= 2) {
+      assert(last_B[B[i1 - 1] - 2] <= N);
+      gt[i1 - 1].push_back(last_B[B[i1 - 1] - 2]);
     }
-    last_B[B[i1] - 1] = i1;
+    last_B[B[i1 - 1] - 1] = i1;
   }
 }
 
-void get_X(unsigned int N, VVU &lt, VU &locations, VU &X, unsigned int current) {
+void get_X(unsigned int N, VVU &gt, VU &locations, VU &X, unsigned int current) {
   if (X[current - 1] < 1) {
-    for (unsigned int i3 = 1; i3 <= lt[current - 1].size(); ++i3)
-      get_X(N, lt, locations, X, lt[current - 1][i3 - 1]);
+    for (unsigned int i3 = 1; i3 <= gt[current - 1].size(); ++i3)
+      get_X(N, gt, locations, X, gt[current - 1][i3 - 1]);
     unsigned int i4;
     for (i4 = 1; i4 <= N && locations[i4 - 1] >= 1; ++i4);
     assert(i4 <= N);
@@ -46,17 +46,17 @@ int main () {
     unsigned int N;
     std::cin >> N;
     VU A(N), B(N);
-    for (unsigned int i2 = 1; i2 <= N; ++i1) std::cin >> A[i2 - 1];
-    for (unsigned int i2 = 1; i2 <= N; ++i1) std::cin >> B[i2 - 1];
-    VVU lt(N);
-    get_lt(N, A, B, lt);
+    for (unsigned int i2 = 1; i2 <= N; ++i2) std::cin >> A[i2 - 1];
+    for (unsigned int i2 = 1; i2 <= N; ++i2) std::cin >> B[i2 - 1];
+    VVU gt(N);
+    get_gt(N, A, B, gt);
     VU X(N, 0); {
       VU locations(N, 0);
-      for (unsigned int i2 = 1; i2 <= N; ++i1) get_X(N, lt, locations, X, i2);
+      for (unsigned int i2 = 1; i2 <= N; ++i2) get_X(N, gt, locations, X, i2);
     }
 
     std::cout << "Case #" << i1 << ":";
-    for (unsigned int i2 = 1; i2 <= N; ++i1) std::cout << " " << X[i2 - 1];
+    for (unsigned int i2 = 1; i2 <= N; ++i2) std::cout << " " << X[i2 - 1];
     std::cout << std::endl;
   }
 

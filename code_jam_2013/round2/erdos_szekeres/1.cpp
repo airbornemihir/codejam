e020aa54bb1a3a84ec26d9dc06c853d337825640
Mihir Mehta <mihir.mehta@samsung.com>
2014-04-14 09:59:05 +0530

    Fixed one obvious problem, and the sequences we get are still correct, but not the lexicographically smallest. So, we're still passing the small test case but not the large one.

--- code_jam_2013/round2/erdos_szekeres/1.cpp
+++ code_jam_2013/round2/erdos_szekeres/1.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <set>
 typedef std::vector<unsigned int> VU;
-typedef std::vector<VU> VVU;
+typedef std::set<unsigned int> SU;
+typedef std::vector<SU> VSU;
 #include <cassert>
 
-void get_gt(unsigned int N, VU &A, VU &B, VVU &gt) {
+void get_gt(unsigned int N, VU &A, VU &B, VSU &gt) {
   VU last_A(N, 0);
   for (unsigned int i1 = 1; i1 <= N; ++i1) {
     if (last_A[A[i1 - 1] - 1] >= 1) 
-      gt[last_A[A[i1 - 1] - 1] - 1].push_back(i1);
+      gt[last_A[A[i1 - 1] - 1] - 1].insert(i1);
     if (A[i1 - 1] >= 2) {
       assert(last_A[A[i1 - 1] - 2] >= 1);
-      gt[i1 - 1].push_back(last_A[A[i1 - 1] - 2]);
+      gt[i1 - 1].insert(last_A[A[i1 - 1] - 2]);
     }
     last_A[A[i1 - 1] - 1] = i1;
   }
   VU last_B(N, N + 1);
   for (unsigned int i1 = N; i1 >= 1; --i1) {
     if (last_B[B[i1 - 1] - 1] <= N)
-      gt[last_B[B[i1 - 1] - 1] - 1].push_back(i1);
+      gt[last_B[B[i1 - 1] - 1] - 1].insert(i1);
     if (B[i1 - 1] >= 2) {
       assert(last_B[B[i1 - 1] - 2] <= N);
-      gt[i1 - 1].push_back(last_B[B[i1 - 1] - 2]);
+      gt[i1 - 1].insert(last_B[B[i1 - 1] - 2]);
     }
     last_B[B[i1 - 1] - 1] = i1;
   }
 }
 
-void get_X(unsigned int N, VVU &gt, VU &locations, VU &X, unsigned int current) {
+void get_X(unsigned int N, VSU &gt, VU &locations, VU &X, unsigned int current) {
   if (X[current - 1] < 1) {
-    for (unsigned int i3 = 1; i3 <= gt[current - 1].size(); ++i3)
-      get_X(N, gt, locations, X, gt[current - 1][i3 - 1]);
+    for (SU::iterator i3 = gt[current - 1].begin();
+	 i3 != gt[current - 1].end();
+	 ++i3)
+      get_X(N, gt, locations, X, *i3);
     unsigned int i4;
     for (i4 = 1; i4 <= N && locations[i4 - 1] >= 1; ++i4);
     assert(i4 <= N);
@@ -48,7 +52,7 @@ int main () {
     VU A(N), B(N);
     for (unsigned int i2 = 1; i2 <= N; ++i2) std::cin >> A[i2 - 1];
     for (unsigned int i2 = 1; i2 <= N; ++i2) std::cin >> B[i2 - 1];
-    VVU gt(N);
+    VSU gt(N);
     get_gt(N, A, B, gt);
     VU X(N, 0); {
       VU locations(N, 0);
